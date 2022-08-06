27f0f297e2a3500b2b315e44da3b93bebe039369
Mike <mykhailo.ovsiannykov@isquad.dev>
2022-08-06 15:30:40 +0300

    Add fetching data

--- screens/home/home-screen/home-screen.js
+++ screens/home/home-screen/home-screen.js
@@ -1,15 +1,34 @@
-import { View, Text } from 'react-native';
-import React, { memo, useState } from 'react';
+import React, { memo, useEffect, useState } from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
 
 import styles from './home-screen.styles';
 
 const HomeScreen = memo(() => {
   const [loading, setLoading] = useState(false);
 
-  const fetchingData = () => {};
+  useEffect(() => {
+    setLoading(true);
+    fetchingData();
+    setLoading(false);
+  }, []);
+
+  const fetchingData = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.log(error));
+  };
+
+  if (loading) {
+    return (
+      <View style={styles.fullScreen}>
+        <ActivityIndicator size="large" color="gray" />
+      </View>
+    );
+  }
 
   return (
-    <View style={styles.screen}>
+    <View style={styles.fullScreen}>
       <Text>HomeScreen</Text>
     </View>
   );
